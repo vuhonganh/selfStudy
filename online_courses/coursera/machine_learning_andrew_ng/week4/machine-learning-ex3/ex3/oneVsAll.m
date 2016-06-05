ca73a78af6a18e1c79aff8df31e744a6a303ea19
Hong Anh VU <vuhonganh91@gmail.com>
2016-06-05 23:43:20 +0200

    done: part 2 of the assignment: implement a multi-classifier for training

--- online_courses/coursera/machine_learning_andrew_ng/week4/machine-learning-ex3/ex3/oneVsAll.m
+++ online_courses/coursera/machine_learning_andrew_ng/week4/machine-learning-ex3/ex3/oneVsAll.m
@@ -4,6 +4,7 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %corresponds to the classifier for label i
 %   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
 %   logisitc regression classifiers and returns each of these classifiers
+
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
 %   to the classifier for label i
 
@@ -49,16 +50,14 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Set options for fmincg
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
-
-
-
+for c = 1:num_labels,
+	theta = zeros(n + 1, 1);
+	[theta] = fmincg(@(t)(lrCostFunction(t, X, (y == c), lambda)), theta, options);
+	all_theta(c,:) = theta';
+end;
 
 % =========================================================================
 
