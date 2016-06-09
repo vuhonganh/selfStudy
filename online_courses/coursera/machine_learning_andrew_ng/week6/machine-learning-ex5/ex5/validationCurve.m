dacbd8c2a8140a6cbf616e9297aea54c0a4f4c66
Hong Anh VU <vuhonganh91@gmail.com>
2016-06-09 20:44:33 +0200

    done: lambda selection part in assignment 5

--- online_courses/coursera/machine_learning_andrew_ng/week6/machine-learning-ex5/ex5/validationCurve.m
+++ online_courses/coursera/machine_learning_andrew_ng/week6/machine-learning-ex5/ex5/validationCurve.m
@@ -39,11 +39,11 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
-
-
-
-
+for i = 1:length(lambda_vec)
+	theta_i = trainLinearReg(X, y, lambda_vec(i));
+	[error_train(i), grad] = linearRegCostFunction(X, y, theta_i, 0);
+	[error_val(i), grad] = linearRegCostFunction(Xval, yval, theta_i, 0);
+end
 
 
 
