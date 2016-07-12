3a8cb2ec8333c7627c52b00cc90ec542a12128cf
Hong Anh VU <vuhonganh91@gmail.com>
2016-07-12 15:40:27 +0100

    update: poker.py : add card_ranks() : using a dictionary: not so nice solution

--- online_courses/udacity/design_comp_prog/lesson_1/poker.py
+++ online_courses/udacity/design_comp_prog/lesson_1/poker.py
@@ -10,6 +10,16 @@ def poker(hands):
 	"""
 	return max(hands, key=hand_rank)
 
+# define a dictionary that contains all the mapping:
+letter_rank = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A':14, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+def card_ranks(cards):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = [letter_rank[r] for r,s in cards]
+    ranks.sort(reverse=True)
+    return ranks
+
+print card_ranks(['AC', '3D', '4S', 'KH']) #should output [14, 13, 4, 3]
+
 
 def hand_rank(hand):
 	"""
