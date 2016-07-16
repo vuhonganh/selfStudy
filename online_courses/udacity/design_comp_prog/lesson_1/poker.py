d765eace7b8bff854c8929d934dacaf97fc2bc72
Hong Anh VU <vuhonganh91@gmail.com>
2016-07-17 00:13:56 +0100

    update: poker.py : add best_ild_hand() : the case we play with Jokers

--- online_courses/udacity/design_comp_prog/lesson_1/poker.py
+++ online_courses/udacity/design_comp_prog/lesson_1/poker.py
@@ -6,12 +6,31 @@
 import random
 import itertools
 
+mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC']
+red_cards = [r+s for r in '23456789TJQKA' for s in 'HD']
+black_cards = [r+s for r in '23456789TJQKA' for s in 'SC']
+
 def best_hand(hand):
     "From a 7-card hand return the best 5-card hand"
     return max(itertools.combinations(hand, 5), key=hand_rank)
-    
 
-mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC']
+
+def replacements(card):
+    "Return a list of all possible replacements for a card. If it is not wild card return itself as a list"
+    if card == '?B': return black_cards
+    elif card == '?R': return red_cards
+    else: return [card]
+
+    
+def best_wild_hand(hand):
+    "Try all value for jokers and return the best 5-card hand. Jokers will be treated as wild cards which can take any rank or suit of the same color."
+    hands = set(best_hand(hand) for hand in itertools.product(*map(replacements, hand)))
+    #Note: the * before map is for unpacking elements
+    #map(func, iterable) = [func(x) for x in iterable]
+    
+    return max(hands, key=hand_rank)
+
+
 def deal(numhands, n=5, deck=mydeck):
     #first shuffle the deck and return each hand the nb of card n 
     random.shuffle(deck)
@@ -172,6 +191,17 @@ def test_best_hand():
             == ['7C', '7D', '7H', '7S', 'JD'])
     return 'test_best_hand passes'
 
+def test_wild_hand():    
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_wild_hand passes'
+
 print test()
 
 print test_best_hand()
+
+print test_wild_hand()
