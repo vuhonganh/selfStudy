c8b80ac63ab17704261e0102ad2a9690f729d016
Hong Anh VU <vuhonganh91@gmail.com>
2016-07-18 03:43:02 +0100

    update: zebra_puzzle.py : add solution and time calculation

--- online_courses/udacity/design_comp_prog/lesson_2/zebra_puzzle.py
+++ online_courses/udacity/design_comp_prog/lesson_2/zebra_puzzle.py
@@ -23,4 +23,71 @@ Each house is painted a different color, and their inhabitants are of different 
 own different pets, drink different beverages and smoke different brands of American cigarettes.
 """
 
+import itertools
 
+def imright(h1, h2):
+    "House h1 is immediately right of h2 if h1-h2 == 1."
+    return h1-h2 == 1
+
+def nextto(h1, h2):
+    "Two houses are next to each other if they differ by 1."
+    return abs(h1-h2) == 1
+
+def zebra_puzzle():
+    "Return a tuple (WATER, ZEBRA indicating their house numbers."
+    houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
+    orderings = list(itertools.permutations(houses)) # 1
+    # use next to return the first feasible solution found
+    return next((WATER, ZEBRA)
+                for (red, green, ivory, yellow, blue) in orderings
+                if imright(green, ivory) # 6
+                for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
+                if Englishman is red  # 2
+                if Norwegian is first # 10
+                if nextto(Norwegian, blue) # 15
+                for (coffee, tea, milk, oj, WATER) in orderings
+                if coffee is green # 4
+                if Ukranian is tea # 5
+                if milk is middle # 9
+                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+                if Kools is yellow # 8
+                if LuckyStrike is oj # 13
+                if Japanese is Parliaments # 14
+                for (dog, snails, fox, horse, ZEBRA) in orderings
+                if Spaniard is dog # 3
+                if OldGold is snails # 7
+                if nextto(Chesterfields, fox) # 11
+                if nextto(Kools, horse) # 12
+                )
+
+print zebra_puzzle()
+
+import time
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers)) 
+
+def timedcalls(n, fn, *args):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time"""
+    # Your code here.
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for _ in range(n)] # variable _ represents a throwaway variable
+    else:
+    	times = []
+    	time_left = n
+    	while time_left >= 0:
+    		one_time = timedcall(fn, *args)[0]
+    		times.append(one_time)
+    		time_left -= one_time
+    return min(times), average(times), max(times)
+
+print timedcalls(1.0, zebra_puzzle)
