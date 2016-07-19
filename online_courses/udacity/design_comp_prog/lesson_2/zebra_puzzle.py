32b61aea002944e8fbee0e4b0fd7cd7d5f59442e
Hong Anh VU <vuhonganh91@gmail.com>
2016-07-19 04:18:45 +0100

    update: zebra_puzzle.py : change all_ints() to a better version

--- online_courses/udacity/design_comp_prog/lesson_2/zebra_puzzle.py
+++ online_courses/udacity/design_comp_prog/lesson_2/zebra_puzzle.py
@@ -110,14 +110,11 @@ def ints(start, end = None):
 
 def all_ints():
     "Generate integers in the order 0, +1, -1, +2, -2, +3, -3, ..."
-    g1 = ints(0)
-    g2 = ints(1)
-    i = 0
+    yield 0
+    i = 1
     while True:
-    	if i < 2 or i % 2 == 1:    		
-    		yield next(g1)
-        else: 
-        	yield 0 - next(g2)
+    	yield i
+    	yield -i
     	i += 1
 
 
