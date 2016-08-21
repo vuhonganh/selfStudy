5328ebcd94e821a407a5e0ef2b4ba6e9b1c148c7
Hong Anh VU <vuhonganh91@gmail.com>
2016-08-21 18:08:43 +0100

    modify :  regex_generator.py : add genseq() : tricky function

--- online_courses/udacity/design_comp_prog/lesson_3/regex_generator.py
+++ online_courses/udacity/design_comp_prog/lesson_3/regex_generator.py
@@ -31,11 +31,27 @@ generated strings so that its length equal exactly the number in Ns.
 
 null = frozenset()
 
-def lit(s):         return lambda Ns: set([s]) if len(s) in Ns else null
+def genseq(x, y, Ns, startx = 0):
+    if not Ns:
+        return null
+    xmatches = x(set(range(startx, max(Ns) + 1)))
+    Ns_x = set(len(m) for m in xmatches)
+    Ns_y = set(n-m for n in Ns for m in Ns_x if n-m >= 0)
+    ymatches = y(Ns_y)
+    return set(m1 + m2 for m1 in xmatches for m2 in ymatches if len(m1 + m2) in Ns)
+
+def lit(s):
+    set_s = set([s]) # precompute this set to avoid repetition each time creating the function
+    return lambda Ns: set_s if len(s) in Ns else null
+
 def alt(x, y):      return lambda Ns: x(Ns) | y(Ns)
 def star(x):        return lambda Ns: opt(plus(x))(Ns)
 def plus(x):        return lambda Ns: genseq(x, star(x), Ns, startx=1) #Tricky
-def oneof(chars):   return lambda Ns: set(chars) if 1 in Ns else null
+
+def oneof(chars):
+    set_chars = set(chars) # precompute this set to avoid repetition each time creating the function
+    return lambda Ns: set_chars if 1 in Ns else null
+
 def seq(x, y):      return lambda Ns: genseq(x, y, Ns)
 def opt(x):         return alt(epsilon, x)
 dot = oneof('?')    # You could expand the alphabet to more chars.
