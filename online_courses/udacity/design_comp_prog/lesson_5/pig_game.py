4eb913d14705853050c630d416f1ddba5fd96e83
hvu <vuhonganh91@gmail.com>
2016-09-03 18:31:09 +0100

    add: pig_game.py : implemented hold(state), roll(state, dice)

---
+++ online_courses/udacity/design_comp_prog/lesson_5/pig_game.py
@@ -0,0 +1,38 @@
+"""
+Game called pig: 2 (or more) players roll the dice to accumulate their score to a goal.
+They can hold or keep rolling (see each function's def.)
+"""
+
+# States are represented as a tuple of (p, me, you, pending) where
+# p:       an int, 0 or 1, indicating which player's turn it is.
+# me:      an int, the player-to-move's current score
+# you:     an int, the other player's current score.
+# pending: an int, the number of points accumulated on current turn, not yet scored
+
+
+def hold(state):
+    """Apply the hold action to a state to yield a new state:
+    Reap the 'pending' points and it becomes the other player's turn."""
+    # once hold: turn changes and next player 's pending = 0
+    return 1 - state[0], state[2], state[1] + state[3], 0
+
+
+def roll(state, d):
+    """Apply the roll action to a state (and a die roll d) to yield a new state:
+    If d is 1, get 1 point (losing any accumulated 'pending' points),
+    and it is the other player's turn. If d > 1, add d to 'pending' points."""
+    if d == 1:
+        return 1 - state[0], state[2], state[1] + 1, 0
+    else:
+        return state[0], state[1], state[2], state[3] + d
+
+
+def test():
+    assert hold((1, 10, 20, 7)) == (0, 20, 17, 0)
+    assert hold((0, 5, 15, 10)) == (1, 15, 15, 0)
+    assert roll((1, 10, 20, 7), 1) == (0, 20, 11, 0)
+    assert roll((0, 5, 15, 10), 5) == (0, 5, 15, 15)
+    return 'tests pass'
+
+
+print test()
