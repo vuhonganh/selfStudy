95525f263ed3791565ce0bd35909d0f488f9e874
hvu <vuhonganh91@gmail.com>
2016-09-06 18:41:14 +0100

    modify: words_game.py: add find_words() in recursive style

--- online_courses/udacity/design_comp_prog/lesson_6/words_game.py
+++ online_courses/udacity/design_comp_prog/lesson_6/words_game.py
@@ -11,20 +11,44 @@ import time
 
 def prefixes(word):
     "A list of the initial sequences of a word, not including the complete word."
-    return [word[:i] for i in range(len(word))]
+    return [word[:i] for i in range(1, len(word))]
 
 
 def readwordlist(filename):
     """Read the words from a file and return a set of the words
-    and a set of the prefixes."""
+    and a set of the prefixes in UPPERCASE."""
     file = open(filename)  # opens file
     text = file.read().upper()  # gets file into string upper
     wordset = set(text.split())
     prefixset = set(p for word in wordset for p in prefixes(word))
+    prefixset.add('')  # add empty string to prefix set
     return wordset, prefixset
 
 
 WORDS, PREFIXES = readwordlist('words4k.txt')
+
+
+def removed(letters, remove):
+    "Return a str of letters, but with each letter in remove removed once."
+    for L in remove:
+        letters = letters.replace(L, '', 1)
+    return letters
+
+
+def find_words(letters):
+    """Return a set of words that can be formed a set of characters from input letters.
+    Note that the formed word must belong to the set WORDS"""
+    results = set()
+
+    def extend_prefix(w, letters):
+        if w in WORDS:
+            results.add(w)
+        if w not in PREFIXES: return
+        for L in letters:
+            extend_prefix(w + L, removed(letters, L))
+
+    extend_prefix('', letters)
+    return results
 
 
 def test():
