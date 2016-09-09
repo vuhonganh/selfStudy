5bdba3b00426854ed9bed14f6be0c13ff83b1275
hvu <vuhonganh91@gmail.com>
2016-09-09 15:54:10 +0100

    modify: anagrams.py: add better solution

--- online_courses/udacity/design_comp_prog/lesson_6/anagrams.py
+++ online_courses/udacity/design_comp_prog/lesson_6/anagrams.py
@@ -13,6 +13,38 @@
 # include 'ARM SAG AN', or 'SAG AN ARM', etc...
 
 
+def anagrams_ver2(phrase, shortest=2):
+    """better version: shorter and more efficient"""
+    return find_anagrams_ver2(phrase.replace(' ', ''), '', shortest)
+
+
+def find_anagrams_ver2(letters, previous_word, shortest):
+    """Using letters, form anagrams using words > previous_word and not shorter than shortest"""
+    result = set()
+    for w in find_words_ver2(letters):
+        if len(w) >= shortest and w >= previous_word:
+            remainder = removed(letters, w)
+            if remainder:
+                for rest in find_anagrams_ver2(remainder, w, shortest):
+                    result.add(w + ' ' + rest)
+            else:
+                result.add(w)
+    return result
+
+
+def find_words_ver2(letters):
+    return extend_prefix_ver2('', letters, set())
+
+
+def extend_prefix_ver2(pre, letters, results):
+    if pre in WORDS:
+        results.add(pre)
+    if pre in PREFIXES:
+        for L in letters:
+            extend_prefix_ver2(pre + L, letters.replace(L, '', 1), results)
+    return results
+
+
 def anagrams(phrase, shortest=2):
     """Return a set of phrases with words from WORDS that form anagram
     of phrase. Spaces can be anywhere in phrase or anagram. All words
@@ -113,6 +145,7 @@ WORDS, PREFIXES = readwordlist('words4k.txt')
 # Run the function test() to see if your function behaves as expected.
 
 def test():
+    print 'File: anagrams.py'
     assert 'DOCTOR WHO' in anagrams('TORCHWOOD')
     assert 'BOOK SEC TRY' in anagrams('OCTOBER SKY')
     assert 'SEE THEY' in anagrams('THE EYES')
@@ -120,6 +153,15 @@ def test():
     assert anagrams('PYTHONIC') == {'NTH PIC YO', 'NTH OY PIC', 'ON PIC THY', 'NO PIC THY', 'COY IN PHT', 'ICY NO PHT',
                                     'ICY ON PHT', 'ICY NTH OP', 'COP IN THY', 'HYP ON TIC', 'CON PI THY', 'HYP NO TIC',
                                     'COY NTH PI', 'CON HYP IT', 'COT HYP IN', 'CON HYP TI'}
+
+    assert 'DOCTOR WHO' in anagrams_ver2('TORCHWOOD')
+    assert 'BOOK SEC TRY' in anagrams_ver2('OCTOBER SKY')
+    assert 'SEE THEY' in anagrams_ver2('THE EYES')
+    assert 'LIVES' in anagrams_ver2('ELVIS')
+    assert anagrams_ver2('PYTHONIC') == {'NTH PIC YO', 'NTH OY PIC', 'ON PIC THY', 'NO PIC THY', 'COY IN PHT', 'ICY NO PHT',
+                                    'ICY ON PHT', 'ICY NTH OP', 'COP IN THY', 'HYP ON TIC', 'CON PI THY', 'HYP NO TIC',
+                                    'COY NTH PI', 'CON HYP IT', 'COT HYP IN', 'CON HYP TI'}
+
     return 'tests pass'
 
 
